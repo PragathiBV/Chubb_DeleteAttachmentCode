981f5c878b9676fdd2a25b02bd70cd90d7290b51
Pragathi BV <PRAGATHI.BV@lntinfotech.com>
2019-04-10 16:13:57 +0530

    Second Commit

---
+++ DeleteQuoteAttachment/DeleteQuoteAttachment/DeleteAttachment.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteQuoteAttachment
+{
+    public class DeleteAttachment : CodeActivity
+    {
+        [Input("Quote ID")]
+
+        [ReferenceTarget("quote")]
+
+        public InArgument<EntityReference> QuoteID { get; set; }
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+
+            //Create the context
+
+            IWorkflowContext context = executionContext.GetExtension<IWorkflowContext>();
+
+            IOrganizationServiceFactory serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
+
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            EntityReference qoute = QuoteID.Get<EntityReference>(executionContext);
+
+            QueryExpression _noteattachmentQuery = new QueryExpression
+            {
+                EntityName = "annotation",
+
+                ColumnSet = new ColumnSet("subject", "filename", "notetext", "documentbody"),
+
+                Criteria = new FilterExpression
+                {
+                    Conditions =
+                    {
+                        new ConditionExpression
+                        {
+                            AttributeName = "objectid",
+                            Operator = ConditionOperator.Equal,
+                            Values = { qoute.Id }
+                        },
+                        //new ConditionExpression
+                        //{
+                        //    AttributeName = "isdocument",
+                        //    Operator = ConditionOperator.Equal,
+                        //    Values = {true}
+                        //}
+                    }
+                }
+
+            };
+            var results = service.RetrieveMultiple(_noteattachmentQuery);
+
+            if (results.Entities.Count > 0)
+            {
+                //we need to fetch Last attachment
+                Entity NotesAttachment = results.Entities.Last();
+
+                foreach (Entity act in results.Entities)
+                {
+                    if (NotesAttachment.Id == act.Id)
+                        continue;
+                    service.Delete(act.LogicalName, act.Id);
+
+                }
+            }
+        }
+    }
+}
---
+++ DeleteQuoteAttachment/DeleteQuoteAttachment/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DeleteQuoteAttachment")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DeleteQuoteAttachment")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d03d7efb-db8f-4bc4-9d46-4b01a1b2eb6e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
